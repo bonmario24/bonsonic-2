4377219f733a853dba99671a6018c431abe8d507
Bonmario <bonmariocontact@gmail.com>
2021-09-03 21:34:00 -0400

    Initial commit

---
+++ s2.h
@@ -0,0 +1,7 @@
+comp_z80_size 0xF4A e für C-Programm */
+#define word_728C_user 0x7298
+#define Obj5F_MapUnc_7240 0x7318
+#define off_3A294 0x3A20C
+#define MapRUnc_Sonic 0x71200
+#define movewZ80CompSize 0xEB04E
+/* Ende Includefile für C-Programm */
